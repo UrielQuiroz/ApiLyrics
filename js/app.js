7c222e91da7c350d97d0998c3e9b8ddd3e007ffb
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-08-17 09:49:40 -0500

    Validando formulario
    Validando que el formulario no este vacío, y si esta vacío muestre un mensaje de error

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import * as UI from './interfaz.js';
+
+UI.frm.addEventListener('submit', buscarCancion);
+
+function buscarCancion(e) {
+    e.preventDefault();
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    //Validando
+    if(artista === '' || cancion === '') {
+        UI.divMsj.textContent = 'Error... Todos los campos son obligatorios';
+        UI.divMsj.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMsj.textContent = '';
+            UI.divMsj.classList.remove();
+        }, 3000);
+
+        remove();
+    }
+}
---
+++ js/interfaz.js
@@ -0,0 +1,5 @@
+export const frm = document.querySelector('#formulario-buscar'),
+             divBuscar = document.querySelector('#buscar'),
+             divMsj = document.querySelector('#mensajes'),
+             divResultado = document.querySelector('#resultado'),
+             headingResult = document.querySelector('.letra-resultado h2');
